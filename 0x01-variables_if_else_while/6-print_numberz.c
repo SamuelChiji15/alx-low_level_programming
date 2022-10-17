05c58c30460d603bce4bfe41f3e45537bda1528d
SamuelChiji15 <samuelchijioke1999@gmail.com>
2022-10-17 15:53:30 -0700

    task6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,20 +6,10 @@
 int main(void)
 
 {
-	char i;
-	char j;
+	int i;
 
-	for (i = 'a'; i <= 'z'; i++)
-
-	{
+	for (i = '0'; i <= '9'; i++)
 		putchar(i);
-	}
-
-	for (j = 'A'; j <= 'Z'; j++)
-
-	{
-		putchar(j);
-	}
 	putchar('\n');
 	return (0);
 }
