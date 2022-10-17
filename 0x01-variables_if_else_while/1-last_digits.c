d147f1c21e9e3c5f45929c7859db2fc115e7f3d6
SamuelChiji15 <samuelchijioke1999@gmail.com>
2022-10-17 15:11:06 -0700

    3

--- 0x01-variables_if_else_while/1-last_digits.c
+++ 0x01-variables_if_else_while/1-last_digits.c
@@ -21,5 +21,6 @@ int main(void)
 			printf("Last digit of %d is %d and is 0\n", n, ldigit);
 		else if (ldigit < 6 && ldigit != 0)
 			printf ("Last digit of %d is %d and is less than 6 and not 0\n" , n, ldigit);
+	}
 		return (0);
 }
