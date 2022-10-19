c83cba4a4da974939501f55235e91ba76971f790
SamuelChiji15 <samuelchijioke1999@gmail.com>
2022-10-19 01:58:08 -0700

    fixed commit

--- 0x02_functions_nested_loops/main.h
+++ 0x02_functions_nested_loops/main.h
@@ -4,7 +4,7 @@ extern int _putchar(char c);
 extern void print_alphabet(void);
 extern void print_alphabet_x10(void);
 extern int _islower(int c);
-extern int _isalpha(int c);
+extern int _isalpha(int c);i
 extern int print_sign(int n);
 extern int _abs(int);
 extern int print_last_digit(int);
