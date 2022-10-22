e5109aead1f2acb24445a6bdaa4ab89c06d2fdce
SamuelChiji15 <samuelchijioke1999@gmail.com>
2022-10-22 07:46:35 -0700

    task6

--- 0x04-more_functions_nested_loops/6-print-line.c
+++ 0x04-more_functions_nested_loops/6-print-line.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- *  * print_line - prints straight line n times.
- * @n: times straight line is printe
+ * print_line - prints straight line n times.
+ * @n: times straight line is printed.
  * Return: no return.
  */
 void print_line(int n)
