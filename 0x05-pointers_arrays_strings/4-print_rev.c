d1735d4f60b09105bc22b98ecb367d2b8c975a30
SamuelChiji15 <samuelchijioke1999@gmail.com>
2022-10-25 09:20:21 -0700

    task4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,14 +6,17 @@
  * Return: Nothing
  */
 
-void print_rev(char *s);
+void print_rev(char *s)
 {
 	int index;
+	{
 
 	for (index = 0; s[index] != '\0'; ++index)
 
 
 	for (--index; index >= 0; --index)
+
 		_putchar (s[index]);
 	_putchar('\n');
+	}
 }
