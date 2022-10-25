bebef042fbc13989140163c3e652c41b0aa77e15
SamuelChiji15 <samuelchijioke1999@gmail.com>
2022-10-25 06:19:37 -0700

    task2

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,16 +8,10 @@
 
 void _puts(char *str)
 {
-	int count = 0;
-
-	while (count >= 0)
+	while (*str != '\0')
 	{
-	if (str[count] == '\n')
-	{
-		_putchar('\n');
-		break;
+		_putchar (*str + 0);
+		++str;
 	}
-		_putchar(str[count]);
-		count++;
-	}
+	_putchar ('\n');
 }
