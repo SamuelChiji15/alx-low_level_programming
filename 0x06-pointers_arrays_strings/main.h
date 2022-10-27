7d17b0f60349c9826b5daa6c24272c30e96adc8d
SamuelChiji15 <samuelchijioke1999@gmail.com>
2022-10-27 07:15:44 -0700

    task101

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,7 +13,7 @@ char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
 int _putchar(char);
-char *infinite_add(char *nl, char *n2, char *r, int size);
+char *infinite_add(char *nl, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
 
