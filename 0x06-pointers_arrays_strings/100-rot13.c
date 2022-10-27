fd5dfdfd3055ade1ccdc7fee1555ec8167ed57f9
SamuelChiji15 <samuelchijioke1999@gmail.com>
2022-10-27 07:13:49 -0700

    fixed

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,27 +1,25 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * rot13 - encoder rot13
- * @s: pointer to string params
- *
- * Return: *s
+ * rot13 - encodes a string using rot13
+ * @s: input string.
+ * Return: the pointer to dest.
  */
 
 char *rot13(char *s)
 {
 	int i;
 	int j;
-	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char *x = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *y = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < 52; j++)
+		for (j = 0; x[j] !='\0'; j++)
 		{
-			if (s[i] == data1[j])
+			if (s[i] == x[j])
 			{
-				s[i] = datarot[j];
+				s[i] = y[j];
 				break;
 			}
 		}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 char *_strcat(char * dest, char *src);
-char *_strcat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
@@ -13,7 +13,7 @@ char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
 int _putchar(char);
-char *infinite_add(char *nl, char *n2, char *r, int size r);
+char *infinite_add(char *nl, char *n2, char *r, int size);
 void print_buffer(char *b, int size);
 
 
