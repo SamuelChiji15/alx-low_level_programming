0c278dc5d69601e3a51c07ad5e7d67b31c410e5b
SamuelChiji15 <samuelchijioke1999@gmail.com>
2022-10-27 06:07:18 -0700

    task12.1

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-//char *_strcat(char * dest, char *src);
-//char *_strcat(char *dest, char *src, int n);
+char *_strcat(char * dest, char *src);
+char *_strcat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
@@ -13,7 +13,7 @@ char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
 int _putchar(char);
-//char *infinite_add(char *nl, char *n2, char *r, int size r);
+char *infinite_add(char *nl, char *n2, char *r, int size r);
 void print_buffer(char *b, int size);
 
 
