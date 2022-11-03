71d6c071658ad3b73ef830ed57058c01e92f9e85
SamuelChiji15 <samuelchijioke1999@gmail.com>
2022-11-03 14:58:33 -0700

    fix

--- 0x08-recursion.c/0-puts_recursion.c
+++ 0x08-recursion.c/0-puts_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-* _puts_recursion - function
-*
-* @s: the chaine
-* Return: Always 0.
-*/
+ * * _puts_recursion - function
+ * *
+ * * @s: the chaine
+ * * Return: Always 0.
+ */
 
 void _puts_recursion(char *s)
 {
@@ -13,7 +13,7 @@ void _puts_recursion(char *s)
 	_putchar('\n');
 	else
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+	_putchar(*s);
+	_puts_recursion(s + 1);
 	}
 }
