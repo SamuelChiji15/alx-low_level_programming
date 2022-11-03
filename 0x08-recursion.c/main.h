dd0a672551c5981d0393907ea3ab19eff960db55
SamuelChiji15 <samuelchijioke1999@gmail.com>
2022-11-03 15:05:58 -0700

    task

--- 0x08-recursion.c/main.h
+++ 0x08-recursion.c/main.h
@@ -1,7 +1,9 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char);
+#include <stdio.h>
+
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -12,4 +14,4 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
-#endif
+#endif /* MAIN_H */
