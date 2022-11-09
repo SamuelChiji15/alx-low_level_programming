bd16c4e48cf22c462abeaa942d4a964d126765d2
SamuelChiji15 <samuelchijioke1999@gmail.com>
2022-11-09 12:56:34 -0800

    create file

---
+++ 0x09-static_libraries/0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 always (success)
+ */
+int main(void)
+
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
